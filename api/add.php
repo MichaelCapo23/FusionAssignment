21f14268f34e093b83cf0db1103330fc1177eab5
MichaelCapo23 <123pie23@gmail.com>
2019-03-16 15:58:10 -0700

    finished add.php needs one last check over

--- api/add.php
+++ api/add.php
@@ -3,7 +3,7 @@
 require_once('connect.php');
 $output = [];
 
-if ($_POST['client']) {
+if ($_POST['ClientName']) {
     $ClientName = $_POST['ClientName'];
     $clientID = $_POST['clientID'];
     $checkForClient = $conn->prepare("SELECT ID 
@@ -12,12 +12,14 @@ if ($_POST['client']) {
     $checkForClient->bind_param("i", $clientID);
     $checkForClient->execute();
     $checkForClient->store_result();
-//    $checkForClient->bind_result() bind the result of query to this variable, don't need to bind it because this query was a simple check.
+//    $checkForClient->bind_result() bind the result of query to a variable, don't need to bind it because this query was a simple check.
     if ($checkForClient->num_rows === 0) {
         $insertNewClient = $conn->prepare("INSERT INTO `clients`
                                                 (`ID`, `name`)
                                                 VALUES (?,?)");
         $insertNewClient->bind_param("is", $clientID, $ClientName);
+        $insertNewClient->execute();
+        $insertNewClient->store_result();
     } else {
         $output['ClientError'][] = 'Client Already Exists';
         print(json_encode($output));
@@ -32,7 +34,9 @@ if ($_POST['sectionName']) {
                                             FROM `clients` 
                                             WHERE ID  = ?");
     $checkForClient->bind_param("i", $clientID);
-    //    $checkForSection->bind_result() bind the result of query to this variable, but I don't need to bind it because this query was a simple check.
+    $checkForClient->execute();
+    $checkForClient->store_result();
+    //    $checkForClient->bind_result() bind the result of query to a variable, but I don't need to bind it because this query was just a simple check.
     if($checkForClient->num_rows !== 0) {
         $checkForSection = $conn->prepare("SELECT ID
                                              FROM `sections`
@@ -40,9 +44,58 @@ if ($_POST['sectionName']) {
         $checkForSection->bind_param("i", $sectionID);
         $checkForSection->execute();
         $checkForSection->store_result();
+        if($checkForSection->num_rows === 0) {
+            $insertNewSection = $conn->prepare("INSERT INTO `sections` 
+                                                (`id`, `client_id`, `name`)
+                                                VALUES (?,?,?)");
+            $insertNewSection->bind_param("iis", $sectionID, $clientID, $sectionName);
+            $insertNewSection->execute();
+            $insertNewSection->store_result();
+        } else {
+            $output['error'][] = 'Section Already Exists';
+            print(json_encode($output));
+        }
     } else {
-        $output['Error'][] = 'Section Already Exists';
+        $output['Error'][] = 'No Corresponding Client';
+        print(json_encode($output));
     }
 }
+
+if($_POST['linkName']) {
+    $sectionID = $_POST['sectionID'];
+    $linkName = $_POST['linkName'];
+    $linkID = $_POST['linkID'];
+    $checkForSection = $conn->prepare("SELECT ID
+                                             FROM `sections`
+                                             WHERE ID = ?");
+    $checkForSection->bind_param("i", $sectionID);
+    $checkForSection->execute();
+    $checkForSection->store_result();
+    //    $checkForSection->bind_result() bind the result of query to a variable, but I don't need to bind it because this query was just a simple check.
+    if($checkForSection->num_rows !== 0) {
+        $checkForLink = $conn->prepare("SELECT id
+                                              FROM `links`
+                                              WHERE id = ?");
+        $checkForLink->bind_param("i", $linkID);
+        $checkForLink->execute();
+        $checkForLink->store_result();
+        if($checkForLink->num_rows === 0) {
+            $insertNewLink = $conn->prepare("INSERT INTO `links` 
+                                            (`id`, `section_id`, `name`)
+                                            VALUES (?,?,?)");
+            $insertNewLink->bind_param("iis", $linkID, $sectionID, $linkName);
+            $insertNewLink->execute();
+            $insertNewLink->store_result();
+        } else {
+            $output['error'][] = 'Link Already Exists';
+            print(json_encode($output));
+        }
+    } else {
+        $output['error'][] = 'No Corresponding Section';
+        print(json_encode($output));
+    }
+}
+
+
 
 ?>
